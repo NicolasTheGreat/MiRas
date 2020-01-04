272752be9596973587c904ae3b6cfbad5aaa52ec
Mikołaj Kamyczek <you@example.com>
2020-01-04 12:59:18 +0100

    base docker app

---
+++ WebAPI/WebAPI/Models/PaymentDetailDBContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAPI.Models
+{
+    public partial class PaymentDetailDBContext : DbContext
+    {
+        public PaymentDetailDBContext()
+        {
+        }
+
+        public PaymentDetailDBContext(DbContextOptions<PaymentDetailDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PaymentDetails> PaymentDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=localhost;Database=PaymentDetailDB;Trusted_Connection=True;User Id=sa;Password=Pa55word2019;Integrated Security=false;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<PaymentDetails>(entity =>
+            {
+                entity.HasKey(e => e.PMId)
+                    .HasName("PK_dbo.PaymentDetails");
+
+                entity.Property(e => e.PMId).HasColumnName("PMId");
+
+                entity.Property(e => e.CardNumber)
+                    .IsRequired()
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CardOwnerName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.CVV)
+                    .IsRequired()
+                    .HasColumnName("CVV")
+                    .HasMaxLength(3)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ExpirationDate)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var server = Configuration["DBServer"] ?? "localhost";
+            var port = Configuration["DBPort"] ?? "1433";
+            var user = Configuration["DBUser"] ?? "SA";
+            var password = Configuration["DBPassword"] ?? "Pa55word2019";
+            var database = Configuration["Database"] ?? "PaymentDetailDB";
+
+            services.AddDbContext<PaymentDetailDBContext>(options =>
+                options.UseSqlServer($"Server={server},{port};Initial Catalog={database};User ID={user};Password={password}"));
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                });
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:8000")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+            app.UseMvc();
+        }
+    }
+}
